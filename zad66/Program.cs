e6bb15b93bc8eb784716a0f2514b290a2ffba482
ipg <>
2023-07-07 22:11:06 +0300

    домашка к семинару 9

---
+++ zad66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Recurse(int number)
+{
+    if (number < 1)
+    {
+        return 0;
+    }
+
+    return number%10 + Recurse(number/10);
+
+}
+
+// ---------------------------------------
+
+int number = ReadInt("Введите число: ");
+
+System.Console.WriteLine(Recurse(Math.Abs(number)));
---
+++ zad68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// m=0 -> n+1
+// m>0, n=0 -> F(m-1,1)
+// m>0, n>0 -> F(m -1, F(m.n-1))
+// проверка по таблице - https://wiki5.ru/wiki/Ackermann_function#Table_of_values 
+
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int akkermanF(int m, int n)
+{
+    if (m == 0)
+    { return n + 1; }
+    else if ((m != 0) && (n == 0))
+    { return akkermanF(m - 1, 1); }
+    else
+    { return akkermanF(m - 1, akkermanF(m, n - 1)); }
+}
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана для {m} и {n} равна {akkermanF(m, n)} ");
